4ced1990376758e6e433c9abef6610309ed98e59
Marc Bogaerts <marcbogaerts@telenet.be>
2016-01-31 13:45:28 +0100

    Ribbon load balancer

--- src/main/java/QuoteClient.java
+++ src/main/java/QuoteClient.java
@@ -2,7 +2,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "quotes", url = "localhost:8090")
+@FeignClient(name = "quotes")
 public interface QuoteClient {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/greetings")
